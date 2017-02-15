33dfe1306405ca76b1063af356853fa27ccb1f49
markdturner <mail.mdturner@gmail.com>
2017-02-15 11:14:09 +0000

    Update functions.php
    Removed line break fix that was causing a break in the TinyMCE editor

--- functions.php
+++ functions.php
@@ -6,24 +6,4 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
-// stop wp removing div tags
-function ikreativ_tinymce_fix( $init )
-{
-    // html elements being stripped
-    $init['extended_valid_elements'] = 'div[*], article[*]';
-
-    // don't remove line breaks
-    $init['remove_linebreaks'] = false;
-
-    // convert newline characters to BR
-    $init['convert_newlines_to_brs'] = true;
-
-    // don't remove redundant BR
-    $init['remove_redundant_brs'] = false;
-
-    // pass back to wordpress
-    return $init;
-}
-add_filter('tiny_mce_before_init', 'ikreativ_tiny_mce_fix');
-
 ?>
