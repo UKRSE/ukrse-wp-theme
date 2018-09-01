70bed3e0728f521118aa8e881d29dbcc116fb515
Stephen Dowsland <s.dowsland@gmail.com>
2018-09-01 10:02:57 +0100

    Add class to html on modal open

--- footer.php
+++ footer.php
@@ -134,13 +134,13 @@ $modalAuthors = $modal.find(".content-modal-inner h3")
 $modalContent = $modal.find(".content-modal-inner .text-content")
 
 hideModal = function () {
-  jQuery("body").removeClass("abstract-modal-open");
+  jQuery("html").removeClass("abstract-modal-open");
   $modal.hide()
   $modalBg.hide()
 }
 
 showModal = function () {
-  jQuery("body").addClass("abstract-modal-open");
+  jQuery("html").addClass("abstract-modal-open");
   $modal.show()
   $modalBg.show()
 }
